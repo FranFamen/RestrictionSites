b98d61dfd1cc8fa9da7bdba9c14b99cf6ce1a083
FranFamen <franciszek.saklawski@gmail.com>
2019-05-12 16:13:43 +0200

    Added separate Class for DNAStrand + amount of specific palindrom existing in the Strand shown in console

--- TetMethylation/Program.cs
+++ TetMethylation/Program.cs
@@ -9,29 +9,30 @@ namespace TetMethylation
     {
         private static List<int> sixCount;
         private static int j = 0;
-        static List<string> sixCharacterWithComplementary, copySixCharacterWithComplementary;
+        static List<string> sixCharacterWithComplementary, sixCharacterWithComplementaryWithRepeated, sixCharacterWithComplementaryWithNumber;
 
         private static string NitrogenousBase { get; set; }
 
         private Program()
         {
-            NitrogenousBase = string.Empty;
+            NitrogenousBase = new DNAStrand().DNA_STRAND;
             sixCharacterWithComplementary = new List<string>();
-            copySixCharacterWithComplementary = new List<string>();
+            sixCharacterWithComplementaryWithRepeated = new List<string>();
+            sixCharacterWithComplementaryWithNumber = new List<string>();
             sixCount = new List<int>();
         }
 
         static void Main(string[] args)
         {
             var nitrobase = new Program();
-            NitrogenousBase = "ATATATATATTATATATACCGATCATGCATGGCATACCGTATA";
+            //Program.CompareSixes(Program.GetSixesWithComplementary(NitrogenousBase));
             Program.GetSixesWithComplementary(NitrogenousBase);
-            Program.CompareSixes(sixCharacterWithComplementary);
+
         }
 
-        private static void GetSixesWithComplementary(string DNAString)
+        private static List<string> GetSixesWithComplementary(string DNAString)
         {
-            //var nitrogenousBase = "CCGATCATGCATGGCATACCGTATA";
+           // var nitrogenousBase = "CCGATCATGCATGGCATACCGTATA";
             var nitrogenousBaseTable = new List<char>();
             var sixCharacters = new List<string>();
             //var nitrobase = new Program();
@@ -59,12 +60,41 @@ namespace TetMethylation
                     complementaryPairs.ContainsKey(charactersInSix[1]) && complementaryPairs[charactersInSix[1]].Equals(charactersInSix[4]) &&
                     complementaryPairs.ContainsKey(charactersInSix[2]) && complementaryPairs[charactersInSix[2]].Equals(charactersInSix[3]))
                 {
+                    sixCharacterWithComplementaryWithRepeated.Add(six);
+                }
+
+            }
+
+            foreach (var six in sixCharacterWithComplementaryWithRepeated)
+            {
+                if (!sixCharacterWithComplementary.Contains(six))
+                {
                     sixCharacterWithComplementary.Add(six);
                 }
             }
+
+            foreach (var six in sixCharacterWithComplementary)
+            {
+                var repetition = 0;
+                foreach (var repeatedSix in sixCharacterWithComplementaryWithRepeated)
+                {
+                    if (repeatedSix == six)
+                    {
+                        repetition++;
+                    }
+                }
+
+                sixCharacterWithComplementaryWithNumber.Add(six + " " + repetition.ToString());
+            }
+
+            foreach (var six in sixCharacterWithComplementaryWithNumber)
+            {
+                Console.WriteLine(six);
+            }
+            return sixCharacterWithComplementary;
         }
     
-        private static void CompareSixes (List <string> Sixer)
+        private static void CompareSixes (List <string> sixer)
         {
             var complementaryArray = new string[sixCharacterWithComplementary.Count];
             sixCharacterWithComplementary.CopyTo(complementaryArray);
@@ -72,8 +102,8 @@ namespace TetMethylation
             var j = 0;
             while (j < sixCharacterWithComplementary.Count)
             {
-                string actual = Sixer[j];
-                sixCount.Add(Sixer.FindAll(six => six.Equals(actual)).Count);
+                string actual = sixer[j];
+                sixCount.Add(sixer.FindAll(six => six.Equals(actual)).Count);
                 var count = 0;
 
                 for (int i = 0; i < length-count; i ++)
